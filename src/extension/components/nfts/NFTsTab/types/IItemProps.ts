cf68e1de93f7216c8ab0e22de9f1c78d5d4f1c8a
Kieran O'Neill <kieran@kibis.is>
2025-05-02 16:40:34 +0100

    chore: squash

---
+++ src/extension/components/nfts/NFTsTab/types/IItemProps.ts
@@ -0,0 +1,9 @@
+// types
+import type { IARC0072AssetHolding, INetwork } from '@extension/types';
+
+interface IItemProps {
+  item: IARC0072AssetHolding;
+  network: INetwork;
+}
+
+export default IItemProps;
