120c281dab997b32c7177a98a13aa37888123a27
Kieran O'Neill <kieran@kibis.is>
2025-05-03 21:58:49 +0100

    fix: reinstate support for use-wallet v2

---
+++ src/middleware/message-brokers/LegacyUseWalletMessageBroker/messages/LegacyUseWalletResponseMessage.ts
@@ -0,0 +1,31 @@
+import { BaseARC0027Error } from '@agoralabs-sh/avm-web-provider';
+
+// enums
+import { LegacyUseWalletMessageReferenceEnum } from '@common/enums';
+
+// types
+import type { ILegacyUseWalletResponseMessage } from '../types';
+
+export default class LegacyUseWalletResponseMessage<Result>
+  implements ILegacyUseWalletResponseMessage<Result>
+{
+  public readonly error: BaseARC0027Error | null;
+  public readonly id: string;
+  public readonly reference: LegacyUseWalletMessageReferenceEnum;
+  public readonly requestId: string;
+  public readonly result: Result | null;
+
+  constructor({
+    error,
+    id,
+    reference,
+    requestId,
+    result,
+  }: ILegacyUseWalletResponseMessage<Result>) {
+    this.error = error;
+    this.id = id;
+    this.reference = reference;
+    this.requestId = requestId;
+    this.result = result;
+  }
+}
