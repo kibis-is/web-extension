d873fd66365dfd61e2ddb5ab82efc1e35492a068
Kieran O'Neill <kieran@kibis.is>
2025-05-03 17:33:51 +0100

    build: update semantic release to use @anolilab/semantic-release-pnpm

--- release.config.mjs
+++ release.config.mjs
@@ -13,19 +13,27 @@ export default {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
-    '@semantic-release/npm',
+    '@anolilab/semantic-release-pnpm',
     [
       '@semantic-release/exec',
       {
-        prepareCmd: './scripts/update_manifest_version.sh ${nextRelease.version} && ./scripts/update_issue_templates.sh ${nextRelease.version}',
-        publishCmd: 'yarn concurrently --names "CHROME,EDGE,FIREFOX,OPERA" -c "yellow.bold,blue.bold,orange.bold,red.bold" "yarn build:chrome && yarn package:chrome" "yarn build:edge && yarn package:edge" "yarn build:firefox && yarn package:firefox" "yarn build:opera && yarn package:opera"',
+        prepareCmd:
+          './scripts/update_manifest_version.sh ${nextRelease.version} && ./scripts/update_issue_templates.sh ${nextRelease.version}',
+        publishCmd:
+          'yarn concurrently --names "CHROME,EDGE,FIREFOX,OPERA" -c "yellow.bold,blue.bold,orange.bold,red.bold" "yarn build:chrome && yarn package:chrome" "yarn build:edge && yarn package:edge" "yarn build:firefox && yarn package:firefox" "yarn build:opera && yarn package:opera"',
       },
     ],
     [
       '@semantic-release/git',
       {
-        assets: ['.github/ISSUE_TEMPLATE/bug_report_template.yml', 'src/manifest.common.json', 'package.json', 'CHANGELOG.md'],
-        message: 'chore(release): ${nextRelease.version}\n\n${nextRelease.notes}',
+        assets: [
+          '.github/ISSUE_TEMPLATE/bug_report_template.yml',
+          'src/manifest.common.json',
+          'package.json',
+          'CHANGELOG.md',
+        ],
+        message:
+          'chore(release): ${nextRelease.version}\n\n${nextRelease.notes}',
       },
     ],
     [
