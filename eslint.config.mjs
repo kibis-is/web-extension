34db41e915d202f72264e732f67c7f513be9833c
Kieran O'Neill <kieran@kibis.is>
2025-05-03 17:40:17 +0100

    build: update eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,47 @@
+import eslint from '@eslint/js';
+import globals from 'globals';
+import prettierConfig from 'eslint-config-prettier';
+import typescriptConfig from 'typescript-eslint';
+
+/**
+ * @type {import('eslint').Linter.Config[]}
+ **/
+export default [
+  eslint.configs.recommended,
+  ...typescriptConfig.configs.recommended,
+  prettierConfig,
+  // custom config
+  {
+    files: ['**/*.{js,mjs,cjs,ts,tsx}'],
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  {
+    ignores: [
+      '.chrome/',
+      '.chrome_build/',
+      '.chrome_profile/',
+      '.dapp_example_build/',
+      '.edge_build/',
+      '.firefox/',
+      '.firefox_build/',
+      '.firefox_profile/',
+      '.opera_build/',
+      'node_modules/',
+    ],
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-inferrable-types': 'off',
+      '@typescript-eslint/no-namespace': ['error', { allowDeclarations: true }],
+      'no-async-promise-executor': 'warn',
+      'prefer-const': 'off',
+    },
+  },
+];
