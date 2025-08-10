adabe044745f79c4fc9fd7ce896705743e70a048
Kieran O'Neill <kieran@kibis.is>
2025-08-10 10:09:50 +0200

    ci: removes firefox and opera from post release builds

--- release.config.mjs
+++ release.config.mjs
@@ -20,7 +20,9 @@ export default {
         prepareCmd:
           './scripts/update_manifest_version.sh ${nextRelease.version} && ./scripts/update_issue_templates.sh ${nextRelease.version}',
         publishCmd:
-          'pnpm concurrently --names "CHROME,EDGE,FIREFOX,OPERA" -c "yellow.bold,blue.bold,orange.bold,red.bold" "pnpm build:chrome && pnpm package:chrome" "pnpm build:edge && pnpm package:edge" "pnpm build:firefox && pnpm package:firefox" "pnpm build:opera && pnpm package:opera"',
+          'pnpm concurrently --names "CHROME,EDGE" -c "yellow.bold,blue.bold" "pnpm build:chrome && pnpm package:chrome" "pnpm build:edge && pnpm package:edge"',
+        // TODO: add firefox and opera when allowed on stores
+        // 'pnpm concurrently --names "CHROME,EDGE,FIREFOX,OPERA" -c "yellow.bold,blue.bold,orange.bold,red.bold" "pnpm build:chrome && pnpm package:chrome" "pnpm build:edge && pnpm package:edge" "pnpm build:firefox && pnpm package:firefox" "pnpm build:opera && pnpm package:opera"',
       },
     ],
     [
